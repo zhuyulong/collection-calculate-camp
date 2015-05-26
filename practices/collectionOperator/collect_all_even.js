622497734ddd4bb0745a2802f2682be3473db31b
zhuyulong <356981944@qq.com>
2015-05-26 12:54:26 +0800

    my second camp

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
+    var _ = require('../lodash/array');
+
+    var evens = _.remove(collection, function(n) {
+    return n % 2 == 0;
+
+    });
+
+    return  evens;
+
 }
 
 module.exports = collect_all_even;
