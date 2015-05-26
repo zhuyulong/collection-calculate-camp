7852c4a2495d0af83260d8d6a026b2479543dadb
zhuyulong <356981944@qq.com>
2015-05-26 13:03:21 +0800

    finsh collect_last_element.js

--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function collect_last_element(collection) {
-  //在这里写入代码
+  var _ = require('../lodash/array');
+
+      return _.last(collection);s
 }
 
 module.exports = collect_last_element;
