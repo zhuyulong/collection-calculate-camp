db5433113e0eed066b8493d5b128e0be2dd6ff81
zhuyulong <356981944@qq.com>
2015-05-27 15:49:59 +0800

    ten coll

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -3,10 +3,24 @@
 function choose_no_repeat_number(collection) {
 
   //在这里写入代码
-  var _ = require('../lodash/array');
-  
-  var array=_.uniq(collection);
+  // var _ = require('../lodash/array');
+  //
+  // var array=_.uniq(collection);
+  //
+  // return array;
+  var array = [];
 
+  for(var i = 0; i < collection.length; i++){
+      var exist = false;
+      for(var j = 0; j < array.length; j++){
+          if(collection[i] === array[j]){
+              exist = true;
+              break;
+          }
+      }if(!exist){
+          array.push(collection[i]);
+      }
+  }
   return array;
 }
 
