6f683e64c646601aeb72259dbda272e27fe463d6
zhuyulong <356981944@qq.com>
2015-05-27 15:51:52 +0800

    one coll

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -2,18 +2,17 @@
 
 function collect_all_even(collection) {
     //var _ = require('../lodash/array');
-    var array=[];
+    var array = [];
     var collection_a = [1, 2, 3, 4, 5];
     var collection_b = [2, 4];
 
-    for(var i=0; i<collection_a.length; i++){
-        for(var j=0; j<collection_b.length; j++){
-            if(collection_a[i] == collection_b[j]){
+    for(var i = 0; i < collection_a.length; i++){
+        for(var j = 0; j < collection_b.length; j++){
+            if(collection_a[i] === collection_b[j]){
                 array.push(collection_a[i]);
             }
         }
     }
-
     return array;
     // var evens = _.remove(collection, function(n) {
     // return n % 2 == 0;
@@ -21,7 +20,6 @@ function collect_all_even(collection) {
     // });
     //
     // return  evens;
-
 }
 
 module.exports = collect_all_even;
