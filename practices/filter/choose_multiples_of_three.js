e1920c8a5efc10d41111dd03fdbf3e71e248a99a
zhuyulong <356981944@qq.com>
2015-05-27 15:11:07 +0800

    nine coll

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -3,11 +3,19 @@
 function choose_multiples_of_three(collection) {
 
   //在这里写入代码
-  var _ = require('../lodash/array');
-  var evens=_.remove(collection, function(n){
-      return n % 3 == 0;
-  });
-  return evens;
+  // var _ = require('../lodash/array');
+  // var evens=_.remove(collection, function(n){
+  //     return n % 3 == 0;
+  // });
+  // return evens;
+  var array=[];
+
+  for(var i=0; i<collection.length; i++){
+      if(collection[i] % 3 == 0){
+          array.push(collection[i]);
+      }
+  }
+  return array;
 }
 
 module.exports = choose_multiples_of_three;
