62e58f19b54410594de2109e76a43ae75fa228eb
zhuyulong <356981944@qq.com>
2015-05-27 09:47:38 +0800

    first coll

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,14 +1,27 @@
 'use strict';
 
 function collect_all_even(collection) {
-    var _ = require('../lodash/array');
+    //var _ = require('../lodash/array');
 
-    var evens = _.remove(collection, function(n) {
-    return n % 2 == 0;
+    var array=[];
+    var collection_a = [1, 2, 3, 4, 5];
+    var collection_b = [2, 4];
 
-    });
+    for(var i=0; i<collection_a.length; i++){
+        for(var j=0; j<collection_b.length; j++){
+            if(collection_a[i] == collection_b[j]){
+                array.push(collection_a[i]);
+            }
+        }
+    }
 
-    return  evens;
+    return array;
+    // var evens = _.remove(collection, function(n) {
+    // return n % 2 == 0;
+    //
+    // });
+    //
+    // return  evens;
 
 }
 
