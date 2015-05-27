d57230a0dc864b3e1fe4c1eb09fe2946e3590b7b
zhuyulong <356981944@qq.com>
2015-05-27 10:30:39 +0800

    three coll

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,25 +1,30 @@
 'use strict';
 
-function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
-  var _ = require('../lodash/array');
+function get_number(number_a,number_b){
+    var array = [];
 
-  var array = [];
+    for(var i = number_a; i <= number_b; i++){
+        array.push(i);
+    }
+    return array;
+}
+function get_number2(number_a,number_b){
+    var array = [];
+    for(var i = number_a; i >= number_b; i--){
+        array.push(i);
+    }
+    return array;
+}
+function get_integer_interval(number_a, number_b) {
+//在这里写入代码
+//var _ = require('../lodash/array');
 
     if(number_a < number_b){
-        for(var i = number_a; i <= number_b; i++){
-            array.push(i);
-        }
-        return array;
+    return get_number(number_a,number_b);
     }
-
     if(number_a > number_b){
-        for(var i = number_a; i >= number_b; i--){
-            array.push(i);
-        }
-        return array;
+    return get_number2(number_a,number_b);
     }
-
     return  [number_a];
 }
 
