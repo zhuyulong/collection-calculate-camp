7d8541d3a949b91a705354b231673f5af11a3752
zhuyulong <356981944@qq.com>
2015-05-27 15:59:48 +0800

    eleven coll

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -2,12 +2,26 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
-  var _ = require('../lodash/array');
+  // var _ = require('../lodash/array');
+  // var array=_.union(collection_a,collection_b);
+    var array=[];
 
-  var array=_.union(collection_a,collection_b);
+    for(var i = 0; i < collection_b.length; i++){
+        collection_a.push(collection_b[i]);
+    }
 
-  return array;
-
+    for(var i = 0; i < collection_a.length; i++){
+        var exist = false;
+        for(var j = 0; j < array.length; j++) {
+             if(collection_a[i] == array[j]) {
+                 exist = true;
+                 break;
+             }
+         }
+        if(!exist){
+             array.push(collection_a[i]);
+         }
+     }
+     return array;
 }
-
  module.exports = get_union;
