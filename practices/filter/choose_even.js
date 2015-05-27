7c8edccb8259b6ccbd5e2eab52632c4e63670b37
zhuyulong <356981944@qq.com>
2015-05-27 14:58:02 +0800

    eight coll

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -3,11 +3,19 @@
 function choose_even(collection) {
 
   //在这里写入代码
-  var _ = require('../lodash/array');
-  var evens=_.remove(collection, function(n){
-      return n % 2 == 0;
-  });
-  return evens;
+  // var _ = require('../lodash/array');
+  // // var evens=_.remove(collection, function(n){
+  // //     return n % 2 == 0;
+  // // });
+  // // return evens;
+  var array=[];
+
+  for(var i=0; i<collection.length; i++){
+      if(collection[i] % 2 == 0){
+          array.push(collection[i]);
+      }
+  }
+  return array;
 }
 
 module.exports = choose_even;
