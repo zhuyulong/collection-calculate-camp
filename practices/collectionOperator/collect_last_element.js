53861dbd2bf891a3427855c0f8d4bbab1e2ad3c1
zhuyulong <356981944@qq.com>
2015-05-27 09:49:07 +0800

    two coll

--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -1,9 +1,10 @@
 'use strict';
 
 function collect_last_element(collection) {
-  var _ = require('../lodash/array');
+  //var _ = require('../lodash/array');
 
-      return _.last(collection);s
+       return(collection.length);
+     // return _.last(collection);
 }
 
 module.exports = collect_last_element;
