038f4a72900347e7f9e3252b88c1721c969422ce
zhuyulong <356981944@qq.com>
2015-05-27 13:21:56 +0800

    six coll

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -2,11 +2,18 @@
 
 function get_intersection(collection_a, collection_b) {
   //在这里写入代码
-  var _ = require('../lodash/array');
+  //var _ = require('../lodash/array');
+  //var array=_.intersection(collection_b,collection_a);
+  //return get_collection(collection_a,collection_b);
+  var array = [];
 
-  var array=_.intersection(collection_b,collection_a);
-
-   return array;
+  for(var i=0; i<collection_b.length; i++){
+      for(var j=0; j<collection_a.length; j++){
+          if(collection_b[i] === collection_a[j]){
+              array[array.length] = collection_b[i];
+          }
+      }
+ }
+ return array;
 }
-
 module.exports = get_intersection;
