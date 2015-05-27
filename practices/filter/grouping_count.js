88d1e3a9c5edfcbe345359fc186ff7bcdde136e9
zhuyulong <356981944@qq.com>
2015-05-27 18:01:15 +0800

    fiveth coll

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,11 +3,17 @@
 function grouping_count(collection) {
 
   //在这里写入代码
-  var _ =require('../lodash/collection');
+//   var _ =require('../lodash/collection');
+// var array=_.countBy(collection, function(n) {
+//      return Math.floor(n);
+//     });
+//     return array;
+    var array = {};
 
- var array=_.countBy(collection, function(n) {
-     return Math.floor(n);
-    });
+    for(var i = 0; i < collection.length; i++){
+        array[collection[i]] = array[collection[i]] ||0;
+        array[collection[i]]++;
+        }
     return array;
 }
 
