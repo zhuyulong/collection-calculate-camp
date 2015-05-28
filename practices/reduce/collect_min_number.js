8d9e39ce42b658b2d2325ae7d79bab7e5b4cfc46
zhuyulong <356981944@qq.com>
2015-05-28 17:12:49 +0800

    25 coll

--- practices/reduce/collect_min_number.js
+++ practices/reduce/collect_min_number.js
@@ -2,7 +2,15 @@
 
 function collect_min_number(collection) {
   //在这里写入代码
+  var min = 0;
+
+  for(var i = 0; i < collection.length; i++){
+     if(collection[min] > collection[i]){
+         collection[min] = collection[i];
+         min = collection[i];
+     }
+  }
+  return collection[min];
 }
 
 module.exports = collect_min_number;
-
