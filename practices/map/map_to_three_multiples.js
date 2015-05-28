b42b9248d988920ec9b482b5e97b14b74ea9bbaf
zhuyulong <356981944@qq.com>
2015-05-28 09:35:40 +0800

    nineth coll

--- practices/map/map_to_three_multiples.js
+++ practices/map/map_to_three_multiples.js
@@ -1,11 +1,17 @@
 'use strict';
 var map_to_three_multiples = function(collections){
+   //
+   //  var _ = require('../lodash/collection');
+   //     return _.map(collections,timesThree);
+   // }
+   //       function timesThree(n) {
+   //           return n * 3;
+   var array = [];
 
-    var _ = require('../lodash/collection');
-       return _.map(collections,timesThree);
+   for(var i = 0; i < collections.length; i++){
+       array.push(collections[i] * 3);
    }
-         function timesThree(n) {
-             return n * 3;
-         }
+   return array;
+}
 
 module.exports = map_to_three_multiples;
