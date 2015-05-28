9ae6b54210878c1be37a9e3628529af64e7da483
zhuyulong <356981944@qq.com>
2015-05-28 16:22:39 +0800

    24 coll

--- practices/map/rank_desc.js
+++ practices/map/rank_desc.js
@@ -1,6 +1,17 @@
 'use strict';
 var rank_desc = function(collection){
-  return [2,3,4,5,6];
-};
+    var temp = 0 ;
+
+    for(var i = 0; i < collection.length; i++){
+        for(var j = i ; j < collection.length; j++){
+            if(collection[i] > collection [j]){
+                temp = collection[i];
+                collection[i] = collection[j];
+                collection[j] = temp;
+            }
+        }
+    }
+    return collection;
+}
 
 module.exports = rank_desc;
