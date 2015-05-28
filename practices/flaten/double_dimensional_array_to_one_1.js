bf2494759195c9a90977b1489f625559182bee01
zhuyulong <356981944@qq.com>
2015-05-28 13:28:29 +0800

    twith coll

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -3,9 +3,21 @@
 function double_to_one(collection) {
 
   //在这里写入代码
-  var _ = require('../lodash/array');
-  var array= _.flatten(collection);
+  // var _ = require('../lodash/array');
+  // var array= _.flatten(collection);
+  //
+  // return array;
+  var array = [];
 
+  for(var i = 0; i < collection.length; i++){
+      if(collection[i].length === undefined){
+          array.push(collection[i]);
+      }else{
+          for(var j = 0 ;j < collection[i].length; j++){
+              array.push(collection[i][j]);
+          }
+      }
+  }
   return array;
 }
 
