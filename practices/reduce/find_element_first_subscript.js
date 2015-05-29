d09bbe0f929c3d083a8924bcd3a097c9c549cd15
zhuyulong <356981944@qq.com>
2015-05-29 22:09:41 +0800

    27 coll

--- practices/reduce/find_element_first_subscript.js
+++ practices/reduce/find_element_first_subscript.js
@@ -2,7 +2,16 @@
 
 function calculate_elements_sum(collection, element) {
   //在这里写入代码
+  var count = 0;
+
+  for(var i = 0; i < collection.length; i++){
+      if(element != collection[i]){
+          count++;
+      }else{
+          break;
+      }
+  }
+  return count;
 }
 
 module.exports = calculate_elements_sum;
-
