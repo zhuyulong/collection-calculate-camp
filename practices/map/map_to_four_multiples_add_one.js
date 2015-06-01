47348e5b4d3d4b2079f7a56803a06e1b901576cd
zhuyulong <356981944@qq.com>
2015-06-01 17:00:16 +0800

    31 coll

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,17 +1,18 @@
 'use strict';
 var map_to_four_multiples_add_one = function(collection){
-   //  var _ = require('../lodash/collection');
-   //     return _.map(collection,timesTwo);
-   //       function timesTwo(n) {
-   //           return (n * 4) + 1;
-   //       }
-   var array = [];
-
-   for(var i = 0; i < collection.length; i++){
-       array.push(collection[i] * 4 + 1);
-   }
-   return array;
-}
+    var map = require('../lodash/zhuyulonglodash/map.js');
+        return map(collection,timesTwo);
+    }
+         function timesTwo(n) {
+             return (n * 4) + 1;
+          }
+//    var array = [];
+//
+//    for(var i = 0; i < collection.length; i++){
+//        array.push(collection[i] * 4 + 1);
+//    }
+//    return array;
+// }
 
 
 
--- practices/map/map_to_three_multiples.js
+++ practices/map/map_to_three_multiples.js
@@ -1,17 +1,17 @@
 'use strict';
 var map_to_three_multiples = function(collections){
-   //
-   //  var _ = require('../lodash/collection');
-   //     return _.map(collections,timesThree);
-   // }
-   //       function timesThree(n) {
-   //           return n * 3;
-   var array = [];
-
-   for(var i = 0; i < collections.length; i++){
-       array.push(collections[i] * 3);
-   }
-   return array;
-}
+     var map = require('../lodash/zhuyulonglodash/map.js');
+        return map(collections,timesThree);
+    }
+      function timesThree(n) {
+              return n * 3;
+      }
+//    var array = [];
+//
+//    for(var i = 0; i < collections.length; i++){
+//        array.push(collections[i] * 3);
+//    }
+//    return array;
+// }
 
 module.exports = map_to_three_multiples;
