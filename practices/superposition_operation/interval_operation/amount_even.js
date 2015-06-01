405eb9892a3d3e5f9283e7df1a0401789e95ccd9
zhuyulong <356981944@qq.com>
2015-06-01 09:56:35 +0800

    30 coll

--- practices/superposition_operation/interval_operation/amount_even.js
+++ practices/superposition_operation/interval_operation/amount_even.js
@@ -3,6 +3,20 @@
 function amount_even(collection) {
 
   //在这里写入代码
+  var number = [];
+
+  for(var i = 0 ; i < collection.length; i++){
+      if(collection[i] %2 ===0){
+          number.push(collection[i]);
+      }
+  }
+
+  var sum = 0;
+
+  for(var j = 0 ; j < number.length; j++){
+      sum +=number[j];
+  }
+  return sum;
 }
 
 module.exports = amount_even;
--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -3,6 +3,22 @@
 function average_uneven(collection) {
 
   //在这里写入代码
+  var number = [];
+
+  for(var i = 0 ; i < collection.length; i++){
+      if(collection[i] %2 !=0){
+          number.push(collection[i]);
+      }
+  }
+
+  var sum = 0;
+  var aver = 0;
+
+  for(var j = 0 ; j < number.length; j++){
+      sum +=number[j];
+  }
+  aver = sum/number.length;
+  return aver;
 }
 
 module.exports = average_uneven;
