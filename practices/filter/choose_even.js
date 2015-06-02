48616d34e8f4caddb0ad79a4a723aa3ddb27fc0e
zhuyulong <356981944@qq.com>
2015-06-02 20:29:55 +0800

    31 coll

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -3,19 +3,20 @@
 function choose_even(collection) {
 
   //在这里写入代码
-  // var _ = require('../lodash/array');
-  // // var evens=_.remove(collection, function(n){
-  // //     return n % 2 == 0;
-  // // });
-  // // return evens;
-  var array=[];
-
-  for(var i=0; i<collection.length; i++){
-      if(collection[i] % 2 == 0){
-          array.push(collection[i]);
-      }
-  }
-  return array;
-}
+  var fiterout = require('../lodash/zhuyulonglodash/fiter.js');
+  var evens = fiterout(collection, function(n){
+      return n % 2 == 0;
+  });
+  return evens;
+ }
+//   var array=[];
+//
+//   for(var i=0; i<collection.length; i++){
+//       if(collection[i] % 2 == 0){
+//           array.push(collection[i]);
+//       }
+//   }
+//   return array;
+// }
 
 module.exports = choose_even;
--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -3,19 +3,20 @@
 function choose_multiples_of_three(collection) {
 
   //在这里写入代码
-  // var _ = require('../lodash/array');
-  // var evens=_.remove(collection, function(n){
-  //     return n % 3 == 0;
-  // });
-  // return evens;
-  var array=[];
-
-  for(var i=0; i<collection.length; i++){
-      if(collection[i] % 3 == 0){
-          array.push(collection[i]);
-      }
-  }
-  return array;
-}
+  var fiterout = require('../lodash/zhuyulonglodash/fiter.js');
+  var evens = fiterout(collection, function(n){
+      return n % 3 == 0;
+  });
+  return evens;
+ }
+//   var array=[];
+//
+//   for(var i=0; i<collection.length; i++){
+//       if(collection[i] % 3 == 0){
+//           array.push(collection[i]);
+//       }
+//   }
+//   return array;
+// }
 
 module.exports = choose_multiples_of_three;
