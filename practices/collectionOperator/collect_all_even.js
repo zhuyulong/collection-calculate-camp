f4a2e6a545b6c9816d59685ce315117f6885f931
zhuyulong <356981944@qq.com>
2015-06-02 21:12:36 +0800

    34 coll

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,25 +1,33 @@
 'use strict';
 
 function collect_all_even(collection) {
-    //var _ = require('../lodash/array');
-    var array = [];
-    var collection_a = [1, 2, 3, 4, 5];
-    var collection_b = [2, 4];
+    var fiter = require('../lodash/zhuyulonglodash/fiter.js');
 
-    for(var i = 0; i < collection_a.length; i++){
-        for(var j = 0; j < collection_b.length; j++){
-            if(collection_a[i] === collection_b[j]){
-                array.push(collection_a[i]);
-            }
+    return fiter(collection,even);
+}
+function even(n) {
+        if (n % 2 ==0) {
+            return n;
         }
-    }
-    return array;
+}
+    // var array = [];
+    // var collection_a = [1, 2, 3, 4, 5];
+    // var collection_b = [2, 4];
+    //
+    // for(var i = 0; i < collection_a.length; i++){
+    //     for(var j = 0; j < collection_b.length; j++){
+    //         if(collection_a[i] === collection_b[j]){
+    //             array.push(collection_a[i]);
+    //         }
+    //     }
+    // }
+    // return array;
     // var evens = _.remove(collection, function(n) {
     // return n % 2 == 0;
     //
     // });
     //
     // return  evens;
-}
+// }
 
 module.exports = collect_all_even;
