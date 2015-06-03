a2c902fb507a49c024558ae0d42648b5353f7ded
zhuyulong <356981944@qq.com>
2015-06-03 16:55:23 +0800

    35 coll

--- practices/reduce/compute_sum.js
+++ practices/reduce/compute_sum.js
@@ -2,11 +2,17 @@
 
 function calculate_elements_sum(collection) {
   //在这里写入代码
-  var sum = 0;
-  for(var i = 0; i < collection.length; i++){
-      sum += collection[i];
-  }
-  return sum ;
+    var _ = require('../lodash/zhuyulonglodash/reduce.js');
+    var sum = _.reduce(collection,function(a,b){
+        return a+b;
+    });
+    return sum;
 }
+//   var sum = 0;
+//   for(var i = 0; i < collection.length; i++){
+//       sum += collection[i];
+//   }
+//   return sum ;
+// }
 
 module.exports = calculate_elements_sum;
