fb952bbaff3d5f9a82b248211d034f70f260ea03
zhuyulong <356981944@qq.com>
2015-06-05 15:29:14 +0800

    39 coll

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -1,19 +1,19 @@
 'use strict';
+var _ = require('../lodash/zhuyulonglodash/zhuyulong.js');
 
 function get_intersection(collection_a, collection_b) {
   //在这里写入代码
-  //var _ = require('../lodash/array');
-  //var array=_.intersection(collection_b,collection_a);
-  //return get_collection(collection_a,collection_b);
-  var array = [];
-
-  for(var i = 0; i < collection_b.length; i++){
-      for(var j = 0; j < collection_a.length; j++){
-          if(collection_b[i] === collection_a[j]){
-              array[array.length] = collection_b[i];
-          }
-      }
- }
- return array;
+    return _.inter(collection_b, collection_a);
 }
+//   var array = [];
+//
+//   for(var i = 0; i < collection_b.length; i++){
+//       for(var j = 0; j < collection_a.length; j++){
+//           if(collection_b[i] === collection_a[j]){
+//               array[array.length] = collection_b[i];
+//           }
+//       }
+//  }
+//  return array;
+// }
 module.exports = get_intersection;
